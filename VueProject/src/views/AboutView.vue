ddde544788e828ead3fbc440f948f85bf18250c0
Lucas Paiva <lucas.paiva@tradeupgroup.com>
2024-04-17 12:47:34 -0300

    init-project
    Signed-off-by: Lucas Paiva <lucas.paiva@tradeupgroup.com>

---
+++ VueProject/src/views/AboutView.vue
@@ -0,0 +1,24 @@
+
+<script setup lang="ts">
+import testItem from '../components/itemTest.vue'
+</script>
+
+<template>
+  <testItem>
+    
+  </testItem>
+
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
